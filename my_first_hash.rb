30a852b0e2bc9b2c88a06d6d2ca62f540eb21d62
Anthony Mai <anthony.mai530@gmail.com>
2019-11-17 03:45:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,7 +6,7 @@ end
 
 
 def shipping_manifest
-  hash = {"whale bone coresets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  hash = {["whale bone coresets" => 5, "porcelain vases" => 2, "oil paintings" => 3]}
 
 
 end
