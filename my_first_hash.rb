491ad2f455461ebf9687a251eb6db054c6474955
Anthony Mai <anthony.mai530@gmail.com>
2019-11-17 03:48:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,9 +19,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  return shipping_manifest["oilpaintings"]
+  shipping_manifest["oilpaintings"]
 end
 
 def adding
